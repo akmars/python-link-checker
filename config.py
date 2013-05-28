e77b35ccba2c441442495345a8609f84df7e87e2
Novytel <makeoffer7@gmail.com>
2013-05-29 06:59:52 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+__author__ = 'mars'
+
+db = {
+  'user': 'root',
+  'password': '',
+  'host': 'localhost',
+  'database': 'links',
+  'raise_on_warnings': True,
+}
+
+site = 'http://site.com'
+
+number_of_links = 200
