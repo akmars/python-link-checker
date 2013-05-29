88eb7904527a323b94a65371dcdc8e48c3f014e5
Novytel <makeoffer7@gmail.com>
2013-05-29 14:50:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 __author__ = 'mars'
+__version__ = "0.1"
+__email__ = "marsel.akhmyednov@gmail.com"
 
 db = {
-  'user': 'root',
-  'password': '',
-  'host': 'localhost',
-  'database': 'links',
-  'raise_on_warnings': True,
+    'user': 'root',
+    'password': '',
+    'host': 'localhost',
+    'database': '',
+    'raise_on_warnings': True,
 }
 
-site = 'http://site.com'
+site = 'http://www.bbc.co.uk/'
 
-number_of_links = 200
+number_of_links = 100000
+# Processing number of links (approximately, depends from the number of links on current page)
